44df1c79279a7194913ee160630c074a49744057
Abhishek Verma <wrmaabhishek@gmail.com>
2023-05-03 21:53:18 +0530

    completed array methods practice

--- 05-Working with arrays/script.js
+++ 05-Working with arrays/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
     <div class="movements__row">
@@ -98,7 +100,6 @@ const createUsername = function (accs) {
 
 createUsername(accounts);
 
-
 // display calculated summary
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
@@ -114,80 +115,114 @@ const calcDisplaySummary = function (acc) {
   const interest = acc.movements
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * acc.interestRate) / 100)
-    .filter((int,i,arr) => 
-    // {
-      // console.log(arr);
-      // return int >= 1;
-      int >= 1
-    // }
+    .filter(
+      (int, i, arr) =>
+        // {
+        // console.log(arr);
+        // return int >= 1;
+        int >= 1
+      // }
     )
     .reduce((acc, mov) => acc + mov, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
 // calcDisplaySummary(account1.movements);
 
-const updateUI = function(acc){
+const updateUI = function (acc) {
   //Display movements
   displayMovements(currAccount.movements);
   //Display balance
   calcDisplayBalance(currAccount);
   //Display summary
   calcDisplaySummary(currAccount);
-}
-
+};
 
 //implementing login
 //eventHandler
 let currAccount;
-btnLogin.addEventListener('click',function(e){
-  //prevent form from submitting automatically 
+btnLogin.addEventListener("click", function (e) {
+  //prevent form from submitting automatically
   e.preventDefault();
 
-  currAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+  currAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
   // console.log(currAccount);
-  if(currAccount?.pin === Number(inputLoginPin.value)){
+  if (currAccount?.pin === Number(inputLoginPin.value)) {
     //Display UI and message
-    labelWelcome.textContent = `Welcome back, ${currAccount.owner.split(' ')[0]}`;
-    containerApp.style.opacity = '100';
+    labelWelcome.textContent = `Welcome back, ${
+      currAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = "100";
     //clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
-    
+
     //updating the UI
     updateUI(currAccount);
-
   }
 });
 
-
 // tranferring money from one acc to other
- btnTransfer.addEventListener('click',function(e){
-      e.preventDefault();
-      const amount = Number(inputTransferAmount.value);
-      const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
-      // console.log(amount,receiverAcc);
-      inputTransferAmount.value = inputTransferTo.value = '';
-
-      if(amount > 0 && receiverAcc && currAccount.balance >= amount && receiverAcc?.username !== currAccount.username){
-        //doing the transfer here
-        currAccount.movements.push(-amount);
-        receiverAcc.movements.push(amount);
-        updateUI(currAccount);
-      }
- })
-
-
- //closing the account
- btnClose.addEventListener('click',function(e){
+btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
-  if(inputCloseUsername.value === currAccount.username && Number(inputClosePin.value) === currAccount.pin){
-    const ind = accounts.findIndex(acc => acc.username === currAccount.username);
-    accounts.splice(ind,1);
-    containerApp.style.opacity = '0';
-  }
-  inputCloseUsername.value = inputClosePin.value = '';
- })
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+  // console.log(amount,receiverAcc);
+  inputTransferAmount.value = inputTransferTo.value = "";
 
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currAccount.balance >= amount &&
+    receiverAcc?.username !== currAccount.username
+  ) {
+    //doing the transfer here
+    currAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+    updateUI(currAccount);
+  }
+});
+
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currAccount.movements.some((mov) => mov >= amount * 0.1)) {
+    //add movement
+    currAccount.movements.push(amount);
+
+    //update UI
+    updateUI(currAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
+//closing the account
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currAccount.username &&
+    Number(inputClosePin.value) === currAccount.pin
+  ) {
+    const ind = accounts.findIndex(
+      (acc) => acc.username === currAccount.username
+    );
+    accounts.splice(ind, 1);
+    containerApp.style.opacity = "0";
+  }
+  inputCloseUsername.value = inputClosePin.value = "";
+});
+
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements(currAccount.movements, !sorted);
+  sorted = !sorted;
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -342,8 +377,6 @@ const maxVal = movements.reduce((acc,mov) => {
 console.log(maxVal);
 */
 
-
-
 /*
 
 // magic of method chaining
@@ -359,7 +392,6 @@ const totalDeposits = movements
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDeposits);
 */
-
 
 /*
 // find method
@@ -381,10 +413,138 @@ console.log(account);
 // console.log(account);
 */
 
-
+/*
 //some and every method
 console.log(movements);
 console.log(movements.includes(-130));
 
-const anyDeposits =  movements.some(mov => mov > 5000);
+// condition
+const anyDeposits =  movements.some(mov => mov > 0);
 console.log(anyDeposits);
+
+
+//every method
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+//separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+
+*/
+
+/*
+//flat and flatmap
+
+const arr = [[1,2,3],[4,5,6],7,8];
+console.log(arr.flat());
+const arrDeep = [[[1,2,3],[4,5,6]],7,8];
+console.log(arrDeep.flat(2));
+
+// const accMovements = accounts.map(acc => acc.movements);
+// const allMovements = accMovements.flat();
+// const overalBalance = allMovements.reduce((acc,mov) => acc + mov,0);
+
+const overalBalance = accounts.map(acc => acc.movements).flat().reduce((acc,mov) => acc + mov,0);
+console.log(overalBalance);
+*/
+
+/*
+// sorting
+//strings
+const owners = ['Abhishek','abhinav','Abhi','shek','jonas','jessica'];
+console.log(owners.sort());
+console.log(owners);
+
+//nubmers
+console.log(movements);
+// console.log(movements.sort()); // will sort elements based on strings
+
+//ascending
+//return < 0 a,b (keep order)
+//return > 0 b,a (switch order)
+// movements.sort((a,b) => a > b ? 1 : -1);
+movements.sort((a,b) => a - b);
+
+console.log(movements);
+
+//descending
+// movements.sort((a,b) => a < b ? 1 : -1);
+movements.sort((a,b) => b - a);
+console.log(movements);
+
+*/
+
+/*
+// some more methods
+const arr = [1,2,3,45];
+
+//empty arrays + fill method
+const x = new Array(7);
+x.fill(1,3,5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23,2,5);
+
+//Array.from
+const y = Array.from({length: 7}, () => 1);
+console.log(y);
+const z = Array.from({length: 10}, (_, i) => i + 1);
+console.log(z);
+const diceRoll = Array.from({length: 100}, () => Math.trunc(Math.random() * 100) + 1);
+console.log(diceRoll);
+
+// //label balance from the UI, get the data directly from UI
+// labelBalance.addEventListener('click',function(){
+//   const movementsUI = Array.from(
+//     document.querySelectorAll('.movements__value'),
+//     el => Number(el.textContent.replace('€',''))
+//     );
+//     console.log(movementsUI);
+//     const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+//     console.log(movementsUI2);
+// });
+
+*/
+
+// array methods practice
+
+//find number of deposits that are greater that 1000
+const numDeposits1000 = accounts
+  .flatMap((acc) => acc.movements)
+  .filter((mov) => mov >= 1000).length;
+
+const numDepWithReduce = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((cnt, mov) => (mov >= 1000 ? cnt + 1 : cnt), 0);
+console.log(numDeposits1000, numDepWithReduce);
+
+//2
+const { deposits, withdrawals } = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? sums.deposits += cur : sums.withdrawals += cur;
+      sums[cur > 0 ? "deposits" : "withdrawals"] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals);
+
+//3rd exercise convert any string into title Case
+const convertTitleCase = function (title) {
+  const exceptions = ["a", "an", "the", "but", "or", "on", "in", "with",'and'];
+  const firstToUp = function(word){
+    return word[0].toUpperCase() + word.slice(1)
+  };
+  const titleCase = title.toLowerCase().split(" ").map(word => exceptions.includes(word) ? word : firstToUp(word)).join(' ');
+  return firstToUp(titleCase);
+};
+
+console.log(convertTitleCase("this is a nice title"));
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with with an EXAMPLE"));
