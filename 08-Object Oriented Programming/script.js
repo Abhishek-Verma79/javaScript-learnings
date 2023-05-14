5c9d9c4bff4f1c078bbc61d59eacf7f64a073698
Abhishek Verma <wrmaabhishek@gmail.com>
2023-05-14 14:54:04 +0530

    completed coding challenge #2

--- 08-Object Oriented Programming/script.js
+++ 08-Object Oriented Programming/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 const Person = function(firstName,birthYear){
     //instance properties
     this.firstName = firstName;
@@ -25,6 +25,12 @@ const jack = new Person('jack',2003);
 console.log(Abhishek,Matilda,jack);
 console.log(Abhishek instanceof Person);
 
+//static method
+Person.hey = function(){
+    console.log('Hey there👋');
+};
+Person.hey();
+// Abhishek.hey();
 //Prototypes
 
 console.log(Person.prototype);
@@ -67,3 +73,117 @@ Array.prototype.unique = function(){
 console.log(arr.unique());
 
 const h1 = document.querySelector('h1'); // check it's protos you can easily see a long prototype chain here
+
+
+// ES6 classes
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl{
+    // this constructor would automatically be called
+    constructor(fullName,birthYear){
+        this.fullName = fullName;
+        this.birthYear = birthYear;
+    }
+
+    // Methods will be added to .prototype property
+    calAge(){
+        console.log(2037 - this.birthYear);
+    }
+
+    greet(){
+        console.log(`hello ${this.firstName}`);
+    }
+
+    get age(){
+        return 2037 - this.birthYear;
+    }
+
+    // set a property that already exists
+    set fullName(name){
+        if(name.includes(' ')) this._fullName = name;
+        else alert(`${name} is not a full name!`);
+    }
+
+    get fullName() {
+        return this._fullName;
+    }
+
+    //static method
+    static hey(){
+        console.log('Hey there👋');
+    }
+}
+
+PersonCl.hey();
+
+// const jessica = new PersonCl('jessica',1991);
+// console.log(jessica);
+// jessica.calAge();
+// console.log(jessica.__proto__);
+// jessica.greet();
+// console.log(jessica.age);
+
+const jd = new PersonCl('jessica davis', 1991);
+console.log(PersonCl.prototype);
+console.log(jd.age);
+console.log(jd.hasOwnProperty('_fullName'));
+const props = Object.getOwnPropertyNames(Object.getPrototypeOf(jd));
+console.log(props);
+console.log(Object.getOwnPropertyNames(jd));
+
+//important things to note about classes
+//1. Classes are not hoisted
+//2. Classes are first class citizens, A func can accept classes ar arguments or return classes
+//3. Classes are executed in strict mode
+
+
+// Getter and setter
+// in normal object
+const account = {
+    owner: 'Jonas',
+    movements: [200,300,10,30],
+
+    get latest(){
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov){
+        this.movements.push(mov);
+    },
+};
+console.log(account);
+console.log(account.latest);
+account.latest = 315;
+console.log(account.movements);
+
+*/
+
+//Object.create
+
+const PersonProto = {
+    calcAge(){
+        return 2037 - this.birthYear;
+    },
+    init(firstName,birthYear){
+        this.firstName = firstName;
+        this.birthYear = birthYear; 
+    }
+};
+
+const steven = Object.create(PersonProto);
+steven.name = 'Steven';
+steven.birthYear = 2003;
+console.log(steven.calcAge());
+console.log(steven);
+console.log(PersonProto);
+
+//verrify things
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah',2002);
+console.log(sarah.calcAge());
+console.log(sarah.firstName);
